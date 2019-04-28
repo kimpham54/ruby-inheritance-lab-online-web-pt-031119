cc4288815b8429ee0733ec79386724615ac918c5
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-28 21:30:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,12 @@ class Student < User
     @knowledge = []
   end
 
-  def learn
+  def learn(string)
+    @knowledge << string
   end
 
-  def knowledge
+  def knowledge(string)
+    @knowledge
   end
   
 end
